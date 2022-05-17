7f220f4214224ed5c9f93d6c8793b9d4cbfdb958
Максим Соколов <sokolov.max@rambler.ru>
2022-05-17 19:19:44 +0300

    Added all classes

---
+++ Oasis.Core/Models/Hall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oasis.Core.Models
+{
+    public class Hall
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+
+        public List<Seat> Seats { get; set; }
+    }
+}
---
+++ Oasis.Core/Models/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oasis.Core.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public DateTime stertTime { get; set; }
+        public int Hours { get; set; }
+        public Seat Seat { get; set; }
+        public double Price { get; set; }
+
+    }
+}
